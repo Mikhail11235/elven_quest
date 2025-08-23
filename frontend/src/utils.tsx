f1c7611bd094e28011843549fa0ea3a09bd63461
Mikhail Posevin <mp@workconsult.com>
2025-08-23 13:00:02 +0300

    Fix allowed tags

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -1,5 +1,5 @@
 export const sanitizeHtml = (html: string) => {
-    const allowedTags = ['p', 'strong', 'em', 'ul', 'ol', 'li', 'a', 'span'];
+    const allowedTags = ['p', 'strong', 'em', 'ul', 'ol', 'li', 'a', 'span', '<br>'];
     const doc = new DOMParser().parseFromString(html, 'text/html');
     doc.body.querySelectorAll('*').forEach(element => {
         if (!allowedTags.includes(element.tagName.toLowerCase())) {
